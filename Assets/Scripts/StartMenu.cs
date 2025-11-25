92df41c71d097826e212e404e3c85773d5f3e59a
HexoKnight <harvey.gream@gmail.com>
2025-11-25 16:55:45 +0000

    feat: finish up start screen MVP

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Eflatun.SceneReference;
+
+public class StartMenu : MonoBehaviour
+{
+    [Tooltip("Transform to rotate for camera pivoting")]
+    public Transform cameraPivot;
+
+    [Tooltip("The speed with which to rotate the camera pivot")]
+    public float pivotSpeed = 10f;
+
+    [Tooltip("The scene to load into on 'Start Game'")]
+    public SceneReference loadScene;
+
+    private AsyncOperation _sceneLoadOperation;
+
+    // was the scene loaded the last time we checked
+    private bool _wasSceneLoaded = false;
+
+    // stops at 90% if `allowSceneActivation` == false:
+    // https://docs.unity3d.com/2023.1/Documentation/ScriptReference/AsyncOperation-progress.html
+    private bool IsSceneLoaded => _sceneLoadOperation.progress >= 0.9f;
+
+    void Start()
+    {
+        _sceneLoadOperation = SceneManager.LoadSceneAsync(loadScene.BuildIndex);
+        _sceneLoadOperation.allowSceneActivation = false;
+    }
+
+    void Update()
+    {
+        if (!_wasSceneLoaded && IsSceneLoaded)
+        {
+            Debug.Log("scene loading finished in the background!");
+            _wasSceneLoaded = true;
+        }
+
+        cameraPivot.RotateAround(cameraPivot.transform.position, cameraPivot.transform.up, pivotSpeed * Time.deltaTime);
+    }
+
+    public void StartGame()
+    {
+        _sceneLoadOperation.allowSceneActivation = true;
+        // TODO: maybe fading
+        if (!IsSceneLoaded)
+        {
+            // loading bar...
+        }
+    }
+
+    public void OpenOptions()
+    {
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
