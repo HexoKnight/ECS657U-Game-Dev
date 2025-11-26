dd811191ebc80e59ca4b5d3e07e7164a1cda83a1
HexoKnight <harvey.gream@gmail.com>
2025-11-26 22:51:00 +0000

    feat: add mouse sensitivity to options menu

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -1,9 +1,27 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour
 {
     [Tooltip("GameObject to make active when 'Back' is pressed")]
     public GameObject prevObject;
+
+    [Tooltip("Slider representing mouse sensitivity")]
+    public Slider mouseSensitivitySlider;
+
+    private void Awake()
+    {
+        mouseSensitivitySlider.value = Options.mouseSensitivity;
+        mouseSensitivitySlider.onValueChanged.AddListener(ChangeMouseSensitivity);
+    }
+
+    public void ChangeMouseSensitivity(float value)
+    {
+        const float MIN = 0.5f;
+        const float MAX = 2.5f;
+
+        Options.mouseSensitivity = (value - mouseSensitivitySlider.minValue) / mouseSensitivitySlider.maxValue * (MAX - MIN) + MIN;
+    }
 
     public void Back()
     {
