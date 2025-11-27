bf1e3bb202b155490984032a77314caea74d36a9
HexoKnight <harvey.gream@gmail.com>
2025-11-27 00:17:42 +0000

    refactor: use fancier system for Options to allow for bidirectional syncing
    for example for loading saved options in the future

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -11,16 +11,29 @@ public class OptionsMenu : MonoBehaviour
 
     private void Awake()
     {
-        mouseSensitivitySlider.value = Options.mouseSensitivity;
-        mouseSensitivitySlider.onValueChanged.AddListener(ChangeMouseSensitivity);
-    }
+        // like standard linear interpolation but t is between t_min and t_max rather than 0 and 1
+        static float Lerp(float t, float t_min, float t_max, float r_min, float r_max) =>
+            (t - t_min) / (t_max - t_min) * (r_max - r_min) + r_min;
 
-    public void ChangeMouseSensitivity(float value)
-    {
-        const float MIN = 0.5f;
-        const float MAX = 2.5f;
+        {
+            // === mouse sensitivity === //
+            const float MIN = 0.5f;
+            const float MAX = 2.5f;
 
-        Options.mouseSensitivity = (value - mouseSensitivitySlider.minValue) / mouseSensitivitySlider.maxValue * (MAX - MIN) + MIN;
+            float sliderToMouseSensitivity(float sliderValue) =>
+                Lerp(sliderValue, mouseSensitivitySlider.minValue, mouseSensitivitySlider.maxValue, MIN, MAX);
+            float mouseSensitivityToSlider(float mouseSensitivity) =>
+                Lerp(mouseSensitivity, MIN, MAX, mouseSensitivitySlider.minValue, mouseSensitivitySlider.maxValue);
+
+            mouseSensitivitySlider.value = mouseSensitivityToSlider(Options.mouseSensitivity);
+            mouseSensitivitySlider.onValueChanged.AddListener(sliderValue =>
+                Options.mouseSensitivity.Value = sliderToMouseSensitivity(sliderValue)
+            );
+            Options.mouseSensitivity.AddListener(mouseSensitivity =>
+                // don't notify or infinite loop
+                mouseSensitivitySlider.SetValueWithoutNotify(mouseSensitivityToSlider(mouseSensitivity))
+            );
+        }
     }
 
     public void Back()
--- Assets/Scripts/Static/Options.cs
+++ Assets/Scripts/Static/Options.cs
@@ -1,4 +1,24 @@
+using UnityEngine.Events;
+
 public static class Options
 {
-    public static float mouseSensitivity = 1.0f;
+    public class WatchableValue<T> : UnityEvent<T>
+    {
+        private T _value;
+        public T Value
+        {
+            get => _value;
+            set
+            {
+                _value = value;
+                Invoke(_value);
+            }
+        }
+
+        private WatchableValue(T value) => _value = value;
+        public static implicit operator WatchableValue<T>(T value) => new(value);
+        public static implicit operator T(WatchableValue<T> watchablevalue) => watchablevalue.Value;
+    }
+
+    public static readonly WatchableValue<float> mouseSensitivity = 1.0f;
 }
