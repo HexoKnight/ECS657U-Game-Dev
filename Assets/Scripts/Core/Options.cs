fce1fbf16da64689ac658fe6d0b547212cb4f57d
HexoKnight <harvey.gream@gmail.com>
2025-12-29 17:02:33 +0000

    revert unnecessary changes
    We don't need another test scene tracked.
    The Options.cs changes are fine but they don't feel necessary,
    especially since lots of them are largely superfluous.
    
    As for the rest, please don't commit these sorts of obviously irrelevant
    files.

--- Assets/Scripts/Core/Options.cs
+++ Assets/Scripts/Core/Options.cs
@@ -1,20 +1,11 @@
 using UnityEngine;
 using UnityEngine.Events;
 
-/// <summary>
-/// Static options container with observable values for settings synchronization.
-/// </summary>
 public static class Options
 {
-    /// <summary>
-    /// Generic observable value that invokes listeners when changed.
-    /// </summary>
-    /// <typeparam name="T">The type of value to watch.</typeparam>
     public class WatchableValue<T> : UnityEvent<T>
     {
         private T _value;
-
-        /// <summary>Gets or sets the value, invoking listeners on change.</summary>
         public T Value
         {
             get => _value;
@@ -30,10 +21,7 @@ public static class Options
         public static implicit operator T(WatchableValue<T> watchablevalue) => watchablevalue.Value;
     }
 
-    /// <summary>Mouse sensitivity multiplier (default 1.0)</summary>
     public static readonly WatchableValue<float> mouseSensitivity = 1.0f;
-
-    /// <summary>Current graphics quality level</summary>
     public static readonly WatchableValue<int> graphicsQuality = QualitySettings.GetQualityLevel();
 
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen)]
